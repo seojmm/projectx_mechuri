cc075b0d0a56b1a75dd313c7ef99355e4713f1c4
seojm <myungs8562@gmail.com>
2023-11-21 12:32:19 +0900

    Fix: assets

--- src/app/recommendation/page.tsx
+++ src/app/recommendation/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+import { recommendState } from "../../atoms/recommendState";
+import { useRecoilState } from "recoil";
+import { Spinner } from "@material-tailwind/react";
 
 export default function RecommendationPage() {
-	return <div>RecommendationPage</div>;
+	const [recommend, setRecommend] = useRecoilState(recommendState);
+
+	if (recommend) {
+		return (
+			<div className="bg-[#0096C4] h-full w-full">
+				<Image src="/추천글자.png" alt="recommend" width="120" height="80" />
+			</div>
+		);
+	} else {
+		return <Spinner className="h-10 w-10" />;
+	}
 }
---
+++ src/atoms/recommendState.js
@@ -0,0 +1,6 @@
+const { atom } = require("recoil");
+
+export const recommendState = atom({
+	key: "recommendState",
+	default: false,
+});
